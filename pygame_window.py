73f7d54756958b3df88e376721322cf2765c1df1
ouda-sadek <ouda.sadek@laplateforme.io>
2025-03-25 13:17:59 +0100

    the inserted numbers are displayed in red

--- pygame_window.py
+++ pygame_window.py
@@ -18,14 +18,18 @@ def display_grid_pygame(sudoku_grid):
                 x, y = col * cell_size, row * cell_size
                 pygame.draw.rect(screen, (BLACK), (x, y, cell_size, cell_size), 2)
                 if sudoku_grid.grid[row][col] != 0:
-                    text = font.render(str(sudoku_grid.grid[row][col]), True, (BLACK))
+                    if sudoku_grid.initial_grid[row][col] == 0:
+                        color = RED
+                    else:
+                        color = BLACK  
+                    text = font.render(str(sudoku_grid.grid[row][col]), True, (color))
                     screen.blit(text, (x + 20, y + 10))
                 
-            # Draw the additional grid lines
+        # Draw the additional grid lines
         for i in range(1, 9):
             if i % 3 == 0:
-                pygame.draw.line(screen, (0, 0, 0), (0, i * cell_size), (540, i * cell_size), 4)  
-                pygame.draw.line(screen, (0, 0, 0), (i * cell_size, 0), (i * cell_size, 540), 4)  
+                pygame.draw.line(screen, (BLACK), (0, i * cell_size), (540, i * cell_size), 4)  
+                pygame.draw.line(screen, (BLACK), (i * cell_size, 0), (i * cell_size, 540), 4)  
 
                 
             
--- script.py
+++ script.py
@@ -3,19 +3,35 @@ class SudokuGrid:
     def __init__(self):
         # The Sudoku grid is initialized as a 9x9 matrix
         self.grid = [[0] * 9 for _ in range(9)]
+        self.initial_grid = [[0] * 9 for _ in range(9)]
 
     # Method to import and parse a grid from a file
     def import_grid(self, filename):
-        with open(filename, "r") as file:
+        with open(filename, 'r') as file:
             lines = file.readlines()
-            for i, line in enumerate(lines):
-                self.grid[i] = [int(c) if c != "_" else 0 for c in line.strip()]
+            for i in range(9):
+                for j in range(9):
+                    num = lines[i][j]
+                    if num != '_':
+                        self.grid[i][j] = int(num)
+                        self.initial_grid[i][j] = int(num)  
 
     # Method to display grid in terminal
     def show_grid(self):
-        for row in self.grid:
-            print(" ".join(str(cell) if cell != 0 else '_' for cell in row))
-
+        for row_idx, row in enumerate(self.grid):
+            row_str = ""
+            for col_idx, cell in enumerate(row):
+                if cell == 0:
+                    row_str += "_ "  
+                else:
+                    # If the box has been filled by the algorithm, display in red
+                    if self.initial_grid[row_idx][col_idx] == 0:
+                        # Display in red if inserted
+                        row_str += f"\033[31m{cell}\033[0m "  
+                    else:
+                        # Normal display if already present
+                        row_str += f"{cell} "  
+            print(row_str)
     # Method to check if a digit is valid in a given position
     def is_valid(self, row, col, num):
         # Check line
